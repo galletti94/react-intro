4c8614434493a6d8ba042c3e722837be64853b99
Lance <lgallett@redhat.com>
2018-11-30 16:19:32 -0500

    Updating: adding ShoppingCart

--- src/App.js
+++ src/App.js
@@ -14,23 +14,45 @@ class Button extends React.PureComponent {
 };
 
 class Counter extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0
-    };
-  }
-  handleClick = () => {
-    this.setState({counter: this.state.counter+1});
-  };
   render() {
+    const {handleUpClick, handleDownClick, handleReset, item, count} = this.props;
     return (
       <div className="ui segment">
-        <h2>Count: {this.state.counter}</h2>
-        <Button onClick={this.handleClick}>+1</Button>
+        <h2>{item}: {count}</h2>
+        <Button onClick={handleUpClick}>+1</Button>
+        <Button onClick={handleDownClick}>-1</Button>
+        <Button onClick={handleReset}>Reset</Button>
       </div>
     )
   }
 };
 
-export {Counter};
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = _.reduce(props.items, (acc, item) => {acc[item] = 0; return acc;}, {});
+  };
+  handleUpClick(item) {
+    this.setState({[item]: this.state[item] + 1})
+  }
+  handleDownClick(item) {
+    this.setState({[item]: this.state[item] - 1})
+  }
+  handleReset(item) {
+    this.setState({[item]: 0})
+  }
+  render() {
+    const {items} = this.props;
+    const total = _.reduce(this.state, (acc, item) => {return acc + item}, 0);
+    return (
+      <React.Fragment>
+        {_.map(items, (item) => <Counter item={item} count={this.state[item]} handleDownClick={() => this.handleDownClick(item)} handleUpClick={() => this.handleUpClick(item)} handleReset={() => this.handleReset(item)} />)}
+        <div className="ui segment">
+          <h2>TOTAL: {total}</h2>
+        </div>
+      </React.Fragment>
+    );
+  }
+};
+
+export {ShoppingCart};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Counter} from './App';
+import {ShoppingCart} from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<ShoppingCart items={["egg", "cheese", "milk"]}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
